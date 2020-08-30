fe8d06bbb26b6bdc560c6e15cce3abb0879561a4
Sebastien Menozzi <seb.menozzi@hotmail.fr>
2020-08-30 16:19:48 +0200

    Minor change

--- camera.py
+++ camera.py
@@ -5,8 +5,8 @@ import cv2
 class Camera(object):
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
-        self.cam.set(3, 1280)
-        self.cam.set(4, 720)
+        self.cam.set(3, 720)
+        self.cam.set(4, 483)
         time.sleep(1)
 
     def __del__(self):
@@ -20,5 +20,8 @@ class Camera(object):
         cv2.putText(img, ts, (20, img.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
             0.6, (0, 0, 255), 1)
 
-        ret, jpeg = cv2.imencode('.jpg', img)
+        new_img = cv2.rotate(img, rotateCode=180)
+
+        ret, jpeg = cv2.imencode('.jpg', new_img)
+
         return jpeg.tostring()
