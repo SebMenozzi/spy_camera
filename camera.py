ffb0a03d60585a7fcb590a077388f9853d80786e
Sebastien Menozzi <seb.menozzi@hotmail.fr>
2020-08-30 23:02:14 +0200

    minor change

--- camera.py
+++ camera.py
@@ -25,8 +25,8 @@ class Camera(BaseCamera):
 
             timestamp = datetime.datetime.now()
             ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-            cv2.putText(img, ts, (16, img.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-                0.6, (255, 255, 255), 1)
+            cv2.putText(img, ts, (20, img.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+                0.4, (255, 255, 255), 1)
 
             # encode as a jpeg image and return it
             yield cv2.imencode('.jpg', img)[1].tobytes()
