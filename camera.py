23df6eede22454ec8e80cbede405262369835715
Sebastien Menozzi <seb.menozzi@hotmail.fr>
2020-08-30 22:41:28 +0200

    minor change

--- camera.py
+++ camera.py
@@ -3,21 +3,15 @@ import datetime
 import cv2
 import imutils
 from base_camera import BaseCamera
+from imutils.video import FileVideoStream
 
 class Camera(BaseCamera):
-    def __init__(self):
-        super(Camera, self).__init__()
-
     @staticmethod
     def frames():
-        camera = cv2.VideoCapture(0)
+        fvs = FileVideoStream(0).start()
 
-        if not camera.isOpened():
-            raise RuntimeError('Could not start camera.')
-
-        while True:
-            # read current frame
-            _, img = camera.read()
+        while fvs.more():
+            img = fvs.read()
 
             # rotate image 180
             img = imutils.rotate(img, 180)
